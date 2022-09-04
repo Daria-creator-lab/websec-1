9de68ebf810e55248aa68d97041d5415df3f4d7f
Daria-creator-lab <pochtadlyunivera@mail.ru>
2022-09-04 14:57:57 +0400

    Работает основной функционал калькулятора

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
-function whenButtonClicked()
-{
+function whenButtonClicked(){
+
     let value1 = document.getElementById("input1").value;
     let value2 = document.getElementById("input2").value;
     let selectAction = document.getElementById("select").value;
     let result = 0;
 
-    if(isNaN (value1)){
+    value1 = isNaN (value1) ?  "Incorrect input" : parseFloat(value1);
+    value2 = isNaN (value2) ?  "Incorrect input" : parseFloat(value2);
 
-        /*console.log(value1, "Incorrect input");
-        value1 = "Incorrect input"
-        document.getElementById("input1").style.color = "#FF0000";
-        return;*/
+    if(value1 == "Incorrect input" || value2 == "Incorrect input")
+    {
+        document.getElementById("results").innerText = "Incorrect input";
+        return;
     }
-    else
-        value1 = parseFloat(value1)
-      
-    
-    value2 = parseFloat(value2);
 
     console.log(value1,value2,selectAction);
 
     switch (selectAction){
         case "plus":
             result = value1 + value2;
+            document.getElementById("results").innerText = String(value1) + "+" + String(value2) + "=" + String(result);
             break
         case "minus":
             result = value1 - value2;
+            document.getElementById("results").innerText = String(value1) + "-" + String(value2) + "=" + String(result);
             break 
         case "mult":
             result = value1 * value2;
+            document.getElementById("results").innerText = String(value1) + "*" + String(value2) + "=" + String(result);
             break
         case "div":
-            if (value2 == 0)
-            {
+            if (value2 == 0){
                 console.log(value2, "Division by zero");
                 result = "Division by zero"
+                document.getElementById("results").innerText = String(result);
             }
-            else 
+            else {
                 result = value1 / value2;
+                document.getElementById("results").innerText = String(value1) + "/" + String(value2) + "=" + String(result);
+            }
             break                
     }
-    
-
-
-    document.getElementById("results").innerText = result;
 }
 
 function changesCheck(fieldName){
@@ -52,5 +49,9 @@ function changesCheck(fieldName){
     let value1 = document.getElementById(fieldName).value;
     
     document.getElementById(fieldName).style.color = (isNaN (value1) ?  "#FF0000" : "#000000");
+
+}
+
+function outputResult(){
 
 }
